e65037e8fc1f9c8ea1d8a78a0afb9bcb6961d94f
apple <apple@MacBook.attlocal.net>
2020-03-27 10:06:25 -0500

    display title...

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Widget(models.Model):
+    description = models.CharField(max_length=100)
+    quantity = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.description}: {self.quantity}"
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Widget
 # Create your views here.
+
+def index(request):
+    widgets = Widget.objects.all()
+    return render(request, 'index.html')
